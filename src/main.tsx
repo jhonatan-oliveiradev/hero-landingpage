326d5909a837090d38ba4f9222fc71b19ab2cff2
JhoNBaratheoN <jhonatan.oliveira931002@gmail.com>
2022-03-17 14:43:11 -0300

    main structure in progress

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./pages/Main";
+
+import GlobalStyle from "./styles/global";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Main />
+    <GlobalStyle />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
